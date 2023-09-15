aa5ded6cdc909c210b3832248f8611671abaeccc
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-09-15 17:11:29 -0300

    Estrutura Básica do Projeto 2 - -04:15

--- relations/routes/api.php
+++ relations/routes/api.php
@@ -3,17 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Um usuário, inicialmente, tem um endereço.
+Route::get('/users', [UserController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/addresses', [AddressController::class, 'index']);
