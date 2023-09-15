a519c7935288cc04c02da6db7e30514cdb895313
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-09-15 10:48:11 -0300

    #5: Entendendo o conceito de Rollback

--- laravel-migrations/database/migrations/2023_09_15_132835_create_products_table.php
+++ laravel-migrations/database/migrations/2023_09_15_132835_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
       $table->string('name');
       $table->string('code')->unique();
       $table->integer('quantity')->default(0);
+      $table->text('obs')->nullable();
       $table->timestamps();
     });
   }
