bb2b1dcc9e15f571a590f8fd0ca35247762ee792
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-09-19 07:27:12 -0300

    #18: Exercicio Invoice

--- relations/app/Http/Controllers/InvoiceController.php
+++ relations/app/Http/Controllers/InvoiceController.php
@@ -7,33 +7,31 @@ use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-  public function index(Request $request) {
+  public function index(Request $request)
+  {
     $invoices = Invoice::all();
     return $invoices;
   }
 
-  public function findOne(Request $request) {
+  public function findOne(Request $request)
+  {
     $invoice = Invoice::find($request->id);
-    if($invoice === null) {
+    if ($invoice === null) {
       return response()->json(['message' => 'Invoice not found'], 404);
     }
     return $invoice;
   }
 
-  public function create(Request $request) {
-    $rawData = $request->only([
+  public function create(Request $request)
+  {
+    $data = $request->only([
       'description',
       'value',
       'address_id',
       'user_id'
     ]);
 
-    $invoice = Invoice::create([
-      'description' => $rawData['description'],
-      'value' => $rawData['value'],
-      'address_id' => $rawData['address_id'],
-      'user_id' => $rawData['user_id']
-    ]);
+    $invoice = Invoice::create($data);
 
     return response()->json($invoice, 201);
   }
