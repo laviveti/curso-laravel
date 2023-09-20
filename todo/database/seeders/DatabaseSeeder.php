f69b85f036d038d180f240985b61f0b96ae23470
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-09-20 08:46:47 -0300

    #13: Introdução as Factories [2]

--- todo/database/seeders/DatabaseSeeder.php
+++ todo/database/seeders/DatabaseSeeder.php
@@ -5,25 +5,20 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Category;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        $this->call([
-            UserSeeder::class,
-            // TaskSeeder::class
-        ]);
-        Category::factory(10)->create();
-        // \App\Models\User::factory(10)->create();
+  // Seed the application's database.
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+  public function run(): void
+  {
+    $this->call([
+      UserSeeder::class,
+    ]);
+
+    Category::factory(10)->create();
+    Task::factory(30)->create();
+  }
 }
