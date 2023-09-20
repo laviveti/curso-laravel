df057007bb11aef66090ad110d7c24a60d17cb76
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-09-20 15:28:28 -0300

    #22: Task List - Estrutura Básica do Template

--- todo/resources/views/layouts/home.blade.php
+++ todo/resources/views/layouts/home.blade.php
@@ -42,6 +42,23 @@
               <option value="1">Todas as tarefas</option>
             </select>
           </div>
+          <div class="task_list">
+            <div class="task">
+
+              <div class="title">
+                <input type="checkbox" />
+                <div class="task_title">Título da tarefa</div>
+              </div>
+              <div class="priority">
+                <div class="sphere"></div>
+                <div>Título da tarefa</div>
+              </div>
+              <div class="actions">
+                Editar - Excluir
+              </div>
+
+            </div>
+          </div>
         </section>
       </main>
     </div>
