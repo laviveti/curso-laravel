427c7c6a3a6741bb94ff725d68703b9f155ed38d
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-09-20 11:56:45 -0300

    #19: Estrutura Básica do Template [2]

--- todo/resources/views/layouts/home.blade.php
+++ todo/resources/views/layouts/home.blade.php
@@ -7,12 +7,26 @@
   <title>TodoApp</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Rubik:wght@400;500&display=swap"
+    rel="stylesheet">
   <link rel="stylesheet" href="/assets/css/style.css" />
 </head>
 
 <body>
-  <section>Hello World</section>
+  <div class="container">
+    <div class="sidebar">Logo</div>
+    <div class="content">
+      <nav>
+        <a href="#" class="btn btn-primary">
+          Criar Tarefa
+        </a>
+      </nav>
+      <main>
+        <section class="graph">grafico</section>
+        <section class="list">lista</section>
+      </main>
+    </div>
+  </div>
 </body>
 
 </html>
