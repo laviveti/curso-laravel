41aa3e2e333bed35447432310aea74d532d1ccc7
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-09-21 16:21:58 -0300

    #38: Criando tela Inclusão de Tasks [2]

--- todo/resources/views/tasks/create.blade.php
+++ todo/resources/views/tasks/create.blade.php
@@ -6,14 +6,28 @@
 
   <section id="create_task_section">
     <h1>Criar tarefa</h1>
-    <div class="inputArea">
-      <label for="title">Título da tarefa:</label>
-      <input type="text" name="title" placeholder="Digite o título da tarefa" required />
-    </div>
-    <div class="inputArea">
-      <label for="title">Título da tarefa:</label>
-      <input type="text" name="title" placeholder="Digite o título da tarefa" required />
-    </div>
+    <form action="">
+      <div class="inputArea">
+        <label for="title">Título da tarefa:</label>
+        <input type="text" id="title" name="title" placeholder="Digite o título da tarefa" required />
+      </div>
+      <div class="inputArea">
+        <label for="due_date">Data de realização:</label>
+        <input type="date" id="due_date" name="due_date" required />
+      </div>
+      <div class="inputArea">
+        <label for="category">Categoria:</label>
+        <select id="category" name="category" required>
+          <div class="select-container"></div>
+          <option value="" selected disabled>Selecione a categoria</option>
+          <option>Valor qualquer</option>
+        </select>
+      </div>
+      <div class="inputArea">
+        <label for="description">Descrição:</label>
+        <textarea id="description" name="description" placeholder="Digite uma descrição para sua tarefa"></textarea>
+      </div>
+    </form>
   </section>
 
 </x-layout>
