8e07a2d3db92512c3d04eaede88118169ad0b901
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-09-21 15:03:33 -0300

    #37: Criando tela inclusão de tasks

--- todo/resources/views/components/task.blade.php
+++ todo/resources/views/components/task.blade.php
@@ -1,12 +1,15 @@
 <div class="task">
+
   <div class="title">
     <input type="checkbox" @if ($task->is_done) checked @endif />
     <div class="task_title">{{ $task->title ?? 'Título da tarefa indisponível' }}</div>
   </div>
+
   <div class="priority">
     <div class="sphere" style="background-color: {{ $task->category->color }}"></div>
     <div>{{ $task->category->title ?? 'Categoria indisponível' }}</div>
   </div>
+
   <div class="actions">
     <a href="{{ route('task.edit', ['id' => $task->id]) }}">
       <img src="/assets/images/icon-edit.png" alt="Editar tarefa">
@@ -15,4 +18,5 @@
       <img src="/assets/images/icon-delete.png" alt="Excluir tarefa">
     </a>
   </div>
+
 </div>
--- todo/resources/views/tasks/create.blade.php
+++ todo/resources/views/tasks/create.blade.php
@@ -1,7 +1,19 @@
 <x-layout pageTitle="B7Web Todo - Criar Tarefa">
+
   <x-slot:btn>
     <a href=" {{ route('home') }} " class="btn btn-primary">Voltar</a>
   </x-slot:btn>
 
-  <h1>Tela de criação de tarefas</h1>
+  <section id="create_task_section">
+    <h1>Criar tarefa</h1>
+    <div class="inputArea">
+      <label for="title">Título da tarefa:</label>
+      <input type="text" name="title" placeholder="Digite o título da tarefa" required />
+    </div>
+    <div class="inputArea">
+      <label for="title">Título da tarefa:</label>
+      <input type="text" name="title" placeholder="Digite o título da tarefa" required />
+    </div>
+  </section>
+
 </x-layout>
