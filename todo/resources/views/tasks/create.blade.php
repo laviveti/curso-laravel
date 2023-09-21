5c26312a0ee46f7aa05e1628ec26a129400794d9
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-09-21 17:12:29 -0300

    #39: Componentizando Formulário (-06:11)

--- todo/resources/views/tasks/create.blade.php
+++ todo/resources/views/tasks/create.blade.php
@@ -5,16 +5,19 @@
   </x-slot:btn>
 
   <section id="create_task_section">
-    <h1>Criar tarefa</h1>
+    <h1>Criar uma nova tarefa</h1>
+
     <form action="">
       <div class="inputArea">
         <label for="title">Título da tarefa:</label>
         <input type="text" id="title" name="title" placeholder="Digite o título da tarefa" required />
       </div>
+
       <div class="inputArea">
         <label for="due_date">Data de realização:</label>
         <input type="date" id="due_date" name="due_date" required />
       </div>
+
       <div class="inputArea">
         <label for="category">Categoria:</label>
         <select id="category" name="category" required>
@@ -23,11 +26,17 @@
           <option>Valor qualquer</option>
         </select>
       </div>
+
       <div class="inputArea">
         <label for="description">Descrição:</label>
         <textarea id="description" name="description" placeholder="Digite uma descrição para sua tarefa"></textarea>
       </div>
+
+      <div class="inputArea">
+        <button class="btn btn-primary" type="submit">Criar tarefa</button>
+      </div>
     </form>
+
   </section>
 
 </x-layout>
