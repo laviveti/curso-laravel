f1020889dc57c712b739053ca35cda6e3aa91240
Thiago Rocha <thiago_srocha01@hotmail.com>
2023-09-20 23:29:32 -0300

    #28: Componentizando Layout

---
+++ todo/resources/views/components/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TodoApp</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Rubik:wght@400;500&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="/assets/css/style.css" />
+</head>
+
+<body>
+  <div class="container">
+    <div class="sidebar">
+      <img src="/assets/images/logo.png" alt="Logo da aplicação" />
+    </div>
+    <div class="content">
+      <nav>
+        <a href="#" class="btn btn-primary">
+          Criar Tarefa
+        </a>
+      </nav>
+      <main>
+        {{$slot}}
+      </main>
+    </div>
+  </div>
+</body>
+
+</html>
