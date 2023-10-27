c745c4bd01e79faf00d93a801e09075994dc99b0
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-10-27 09:25:28 -0300

    #70: Tradução e Exibição de Datas [Carbon]

--- todo/app/Http/Controllers/HomeController.php
+++ todo/app/Http/Controllers/HomeController.php
@@ -10,22 +10,29 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-  public function index(Request $request){
+  public function index(Request $request)
+  {
     $filteredDate = $request->date;
 
-    if(!$request->date) {
+    if (!$request->date) {
       $filteredDate = date('Y-m-d');
     }
-
-    $carbonDate = Carbon::createFromDate($filteredDate);
+    $carbonDate = Carbon::createFromDate($filteredDate); // today = (27)
 
     $data['tasks'] = Task::whereDate('due_date', date('Y-m-d'))->get();
     $data['authUser'] = Auth::user();
     $data['tasks_count'] = $data['tasks']->count();
     $data['undone_tasks_count'] = $data['tasks']->where('is_done', false)->count();
-    $data['date_as_string'] = $carbonDate->format('d \d\e M');
-    $data['date_prev_button'] = $carbonDate->addDay(-1)->format('Y-m-d');
-    $data['date_next_button'] = $carbonDate->addDay(2)->format('Y-m-d');
+
+    // Dividindo a data em parte
+    $dateParts = explode(' ', $carbonDate->translatedFormat('d \d\e M'));
+    // Convertendo a primeira letra do mês para maiúscula
+    $dateParts[2] = ucfirst($dateParts[2]);
+    // Juntando as partes de volta
+    $data['date_as_string'] = implode(' ', $dateParts); // '00 de Mês'
+
+    $data['date_prev_button'] = $carbonDate->addDay(-1)->format('Y-m-d'); // today = (26)
+    $data['date_next_button'] = $carbonDate->addDay(2)->format('Y-m-d');  // today = (28)
 
 
     return view('home', $data);
