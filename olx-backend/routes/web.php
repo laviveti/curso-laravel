c21bde6eaa28853bfc6ba276294627f061e84545
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-10-31 16:36:54 -0300

    #15: Planejando as Rotas

--- olx-backend/routes/web.php
+++ olx-backend/routes/web.php
@@ -2,17 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Utils routes
+// [] - /ping
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Auth routes
+// [] - /user/signin
+// [] - /user/signup
+// [] - /user/me
+
+// Config routes
+// [] - /states
+// [] - /categories
+
+// Advertises routes
+// [] - /ad/list
+// [] - /ad/:id
+// [] - /ad/add
+// [] - /ad/:id(PUT)
+// [] - /ad/:id(DELETE)
+// [] - /ad/:id/:image(DELETE)
