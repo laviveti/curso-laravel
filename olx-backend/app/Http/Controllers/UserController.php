db3a1deaca029b0c24424ac5257104f2910d2a06
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-01 11:31:59 -0300

    #23- Segurança de Senhas e Registro

--- olx-backend/app/Http/Controllers/UserController.php
+++ olx-backend/app/Http/Controllers/UserController.php
@@ -7,16 +7,19 @@ use App\Http\Requests\CreateUserRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
   public function signup(CreateUserRequest $request): JsonResponse
   {
     $data = $request->only(['name', 'email', 'password', 'state_id']);
+    $data['password'] = Hash::make($data['password']);
     $user = User::create($data);
+
     $response = [
       'error' => '',
-      'user' => $user
+      'token' => $user->createToken('register_token')->plainTextToken
     ];
 
     return response()->json($response);
