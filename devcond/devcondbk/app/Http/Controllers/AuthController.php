6cf4f722242e5ce4be3670494a1c41c9b979d8af
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-03 16:37:15 -0300

    don't repeat yourself

--- devcond/devcondbk/app/Http/Controllers/AuthController.php
+++ devcond/devcondbk/app/Http/Controllers/AuthController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Unit;
 use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
-
 
 class AuthController extends Controller
 {
@@ -19,11 +19,38 @@ class AuthController extends Controller
     ], 401);
   }
 
+  private function validateRequest(Request $request, array $rules)
+  {
+    $validator = Validator::make($request->all(), $rules);
+
+    if ($validator->fails()) {
+      return $validator->errors()->first();
+    }
+
+    return null;
+  }
+  private function createToken(array $credentials)
+  {
+    $token = auth()->attempt($credentials);
+
+    if (!$token) {
+      return null;
+    }
+
+    return $token;
+  }
+  private function getUnitProperties(Authenticatable $user)
+  {
+    return Unit::select(['id', 'name'])
+      ->where('id_owner', $user['id'])
+      ->get();
+  }
+
   public function register(Request $request)
   {
     $array = ['error' => ''];
 
-    $validator = Validator::make($request->all(), [
+    $error = $this->validateRequest($request, [
       'name' => 'required',
       'email' => 'required|email|unique:users,email',
       'cpf' => 'required|digits:11|unique:users,cpf',
@@ -31,7 +58,7 @@ class AuthController extends Controller
       'password_confirm' => 'required|same:password'
     ]);
 
-    if (!$validator->fails()) {
+    if (!$error) {
       $name = $request->input('name');
       $email = $request->input('email');
       $cpf = $request->input('cpf');
@@ -46,7 +73,7 @@ class AuthController extends Controller
       $newUser->password = $hash;
       $newUser->save();
 
-      $token = auth()->attempt([
+      $token = $this->createToken([
         'cpf' => $cpf,
         'password' => $password
       ]);
@@ -61,15 +88,52 @@ class AuthController extends Controller
       $user = auth()->user();
       $array['user'] = $user;
 
-      $properties = Unit::select(['id', 'name'])
-        ->where('id_owner', $user['id'])
-        ->get();
+      $properties = $this->getUnitProperties($user);
 
       $array['user']['properties'] = $properties;
 
 
     } else {
-      $array['error'] = $validator->errors()->first();
+      $array['error'] = $error;
+    }
+
+    return $array;
+  }
+  public function login(Request $request)
+  {
+    $array = ['error' => ''];
+
+    $error = $this->validateRequest($request, [
+      'cpf' => 'required|digits:11',
+      'password' => 'required'
+    ]);
+
+    if (!$error) {
+      $cpf = $request->input('cpf');
+      $password = $request->input('password');
+
+      $token = $this->createToken([
+        'cpf' => $cpf,
+        'password' => $password
+      ]);
+
+      if (!$token) {
+        $array['error'] = 'CPF e/ou senha estão incorretos.';
+        return $array;
+      }
+
+      $array['token'] = $token;
+
+      $user = auth()->user();
+      $array['user'] = $user;
+
+      $properties = $this->getUnitProperties($user);
+
+      $array['user']['properties'] = $properties;
+
+    } else {
+      $array['errors'] = $error;
+      return $array;
     }
 
     return $array;
