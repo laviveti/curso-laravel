49aec35968cb6638280a09dd5ef631e199706524
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-03 08:30:59 -0300

    #5 - Criando as Migrations 2

--- devcond/devcondbk/database/migrations/2023_11_03_103648_create_all_tables.php
+++ devcond/devcondbk/database/migrations/2023_11_03_103648_create_all_tables.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 class CreateAllTables extends Migration
 {
   public function up(): void
@@ -70,6 +72,47 @@ class CreateAllTables extends Migration
       $table->string('title');
       $table->string('file_url');
     });
+
+    Schema::create('warnings', function (Blueprint $table) {
+      $table->id();
+      $table->integer('id_unit');
+      $table->string('title');
+      $table->string('status')->default('IN_REVIEW'); // IN_REVIEW, RESOLVED
+      $table->date('date_created');
+      $table->text('photos');
+    });
+
+    Schema::create('lost_and_found', function (Blueprint $table) {
+      $table->id();
+      $table->string('status')->default('LOST'); // LOST, RECOVERED
+      $table->string('photo');
+      $table->string('description');
+      $table->string('where');
+      $table->date('date_created');
+    });
+
+    Schema::create('areas', function (Blueprint $table) {
+      $table->id();
+      $table->integer('allowed')->default(1);
+      $table->string('title');
+      $table->string('cover');
+      $table->string('days'); // 0,1,2,3,4,5,6
+      $table->time('start_time');
+      $table->time('end_time');
+    });
+
+    Schema::create('area_disabled_days', function (Blueprint $table) {
+      $table->id();
+      $table->integer('id_area');
+      $table->date('day');
+    });
+
+    Schema::create('reservations', function (Blueprint $table) {
+      $table->id();
+      $table->integer('id_unit');
+      $table->integer('id_area');
+      $table->dateTime('reservation_date');
+    });
   }
 
   /**
@@ -77,7 +120,20 @@ class CreateAllTables extends Migration
    */
   public function down(): void
   {
-
+    Schema::dropIfExists('users');
+    schema::dropIfExists('units');
+    Schema::dropIfExists('unit_people');
+    Schema::dropIfExists('unit_vehicles');
+    Schema::dropIfExists('unit_pets');
+    Schema::dropIfExists('walls');
+    Schema::dropIfExists('wall_likes');
+    Schema::dropIfExists('docs');
+    Schema::dropIfExists('billets');
+    Schema::dropIfExists('warnings');
+    Schema::dropIfExists('lost_and_found');
+    Schema::dropIfExists('areas');
+    Schema::dropIfExists('area_disabled_days');
+    Schema::dropIfExists('reservations');
   }
 }
 ;
