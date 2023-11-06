7b4391fcbca2a884c455fbf58ad6b3d3ec5db35d
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-06 11:11:45 -0300

    #17 - /docs

--- devcond/devcondbk/app/Http/Controllers/DocController.php
+++ devcond/devcondbk/app/Http/Controllers/DocController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doc;
 use Illuminate\Http\Request;
 
 class DocController extends Controller
 {
-    //
+  public function getAll()
+  {
+    $array = ['error' => '', 'list' => []];
+
+    $docs = Doc::all();
+
+    foreach ($docs as $docKey => $docValue) {
+      $docs[$docKey]['file_url'] = asset('storage/' . $docValue['file_url']);
+    }
+
+    $array['list'] = $docs;
+
+    return $array;
+  }
 }
