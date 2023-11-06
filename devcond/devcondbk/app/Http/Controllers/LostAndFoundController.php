26e5a35d84eb350a92bcf9643ccda7dabe1b1108
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-06 16:53:42 -0300

    #23 - /foundandlost (get)

--- devcond/devcondbk/app/Http/Controllers/LostAndFoundController.php
+++ devcond/devcondbk/app/Http/Controllers/LostAndFoundController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LostAndFound;
 use Illuminate\Http\Request;
 
 class LostAndFoundController extends Controller
 {
-    //
+  public function getAll()
+  {
+    $array = ['error' => ''];
+
+    $lost = LostAndFound::where('status', 'LOST')
+      ->orderBy('date_created', 'DESC')
+      ->orderBy('id', 'DESC')
+      ->get();
+
+    $recovered = LostAndFound::where('status', 'RECOVERED')
+      ->orderBy('date_created', 'DESC')
+      ->orderBy('id', 'DESC')
+      ->get();
+
+    foreach ($lost as $lostKey => $lostValue) {
+      $lost[$lostKey]['date_created'] = date('d/m/Y', strtotime($lostValue['date_created']));
+      $lost[$lostKey]['photo'] = asset('storage/', $lostValue['photo']);
+    }
+
+    foreach ($recovered as $recKey => $recValue) {
+      $lost[$recKey]['date_created'] = date('d/m/Y', strtotime($recValue['date_created']));
+      $lost[$recKey]['photo'] = asset('storage/', $recValue['photo']);
+    }
+
+
+
+    $array['lost'] = $lost;
+    $array['recovered'] = $recovered;
+
+    return $array;
+  }
 }
