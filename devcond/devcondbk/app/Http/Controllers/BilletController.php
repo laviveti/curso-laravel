a9987950938d2992345f4f74068b35a82ff39f56
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-06 13:43:28 -0300

    #18 - /billets

--- devcond/devcondbk/app/Http/Controllers/BilletController.php
+++ devcond/devcondbk/app/Http/Controllers/BilletController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Billet;
+use App\Models\Unit;
 use Illuminate\Http\Request;
 
 class BilletController extends Controller
 {
-    //
+  public function getAll(Request $request)
+  {
+    $array = ['error' => '', 'list' => []];
+
+    $property = $request->input('property');
+    if ($property) {
+      $user = auth()->user();
+
+      $unit = Unit::where('id', $property)->where('id_owner', $user['id'])
+        ->count();
+
+      if ($unit > 0) {
+        $billets = Billet::where('id_unit', $property)->get();
+
+        foreach ($billets as $billetKey => $billetValue) {
+          $billets[$billetKey]['file_url'] = asset('storage/' . $billetValue['file_url']);
+        }
+
+        $array['list'] = $billets;
+      } else {
+        $array['error'] = 'A unidade não é sua';
+      }
+
+    } else {
+      $array['error'] = 'A propriedade é necessária';
+    }
+
+    return $array;
+  }
 }
