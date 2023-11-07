b00dead1b66848b59426e58690da438941260981
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-07 08:55:03 -0300

    #25: /unit/{id}

--- devcond/devcondbk/app/Http/Controllers/LostAndFoundController.php
+++ devcond/devcondbk/app/Http/Controllers/LostAndFoundController.php
@@ -68,4 +68,26 @@ class LostAndFoundController extends Controller
     }
     return $array;
   }
+
+  public function update($id, Request $request)
+  {
+    $array = ['error' => ''];
+
+    $status = $request->input('status');
+    if ($status && in_array($status, ['lost', 'recovery'])) {
+      $item = LostAndFound::find($id);
+      if ($item) {
+        $item->status = $status;
+        $item->save();
+      } else {
+        $array['error'] = 'Produto inexistente';
+        return $array;
+      }
+    } else {
+      $array['error'] = 'Status não existe';
+      return $array;
+    }
+
+    return $array;
+  }
 }
--- devcond/devcondbk/app/Http/Controllers/UnitController.php
+++ devcond/devcondbk/app/Http/Controllers/UnitController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
+use App\Models\UnitPeople;
+use App\Models\UnitPet;
+use App\Models\UnitVehicle;
 use Illuminate\Http\Request;
 
 class UnitController extends Controller
 {
-    //
+  public function getInfo($id)
+  {
+    $array = ['error' => ''];
+
+    $unit = Unit::find($id);
+    if ($unit) {
+      $people = UnitPeople::where('id_unit', $id)->get();
+      $vehicles = UnitVehicle::where('id_unit', $id)->get();
+      $pets = UnitPet::where('id_unit', $id)->get();
+
+      foreach ($people as $perKey => $perValue) {
+        $people[$perKey]['birthdate'] = date('d/m/Y', strtotime($perValue['birthdate']));
+      }
+
+      $array['people'] = $people;
+      $array['vehicles'] = $vehicles;
+      $array['pets'] = $pets;
+    } else {
+      $array['error'] = 'Propriedade inexistente';
+      return $array;
+    }
+
+    return $array;
+  }
 }
