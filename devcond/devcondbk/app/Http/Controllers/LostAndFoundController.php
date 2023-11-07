0f4a672aca579abf60f109f6e53c396747a6ed86
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2023-11-07 08:03:48 -0300

    #24 - /foundandlost (post)

--- devcond/devcondbk/app/Http/Controllers/LostAndFoundController.php
+++ devcond/devcondbk/app/Http/Controllers/LostAndFoundController.php
@@ -24,12 +24,12 @@ class LostAndFoundController extends Controller
 
     foreach ($lost as $lostKey => $lostValue) {
       $lost[$lostKey]['date_created'] = date('d/m/Y', strtotime($lostValue['date_created']));
-      $lost[$lostKey]['photo'] = asset('storage/', $lostValue['photo']);
+      $lost[$lostKey]['photo'] = asset('storage/' . $lostValue['photo']);
     }
 
     foreach ($recovered as $recKey => $recValue) {
-      $lost[$recKey]['date_created'] = date('d/m/Y', strtotime($recValue['date_created']));
-      $lost[$recKey]['photo'] = asset('storage/', $recValue['photo']);
+      $recovered[$recKey]['date_created'] = date('d/m/Y', strtotime($recValue['date_created']));
+      $recovered[$recKey]['photo'] = asset('storage/' . $recValue['photo']);
     }
 
     $array['lost'] = $lost;
@@ -45,16 +45,27 @@ class LostAndFoundController extends Controller
     $validator = Validator::make($request->all(), [
       'description' => 'required',
       'where' => 'required',
-      'photo' => 'required',
+      'photo' => 'required|file|mimes:png,jpg',
     ]);
 
     if (!$validator->fails()) {
       $description = $request->input('description');
       $where = $request->input('where');
+      $file = $request->file('photo')->store('public');
+      $file = explode('public/', $file);
+      $photo = $file[1];
+
+      $newLost = new LostAndFound();
+      $newLost->status = 'LOST';
+      $newLost->photo = $photo;
+      $newLost->description = $description;
+      $newLost->where = $where;
+      $newLost->date_created = date('Y-m-d');
+      $newLost->save();
     } else {
-
+      $array['error'] = $validator->errors()->first();
+      return $array;
     }
-
     return $array;
   }
 }
